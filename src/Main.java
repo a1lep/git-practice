aeffa072b223eccf125ddf166a972db10290a963
a1lep <aile.pent@gmail.com>
2024-11-12 11:40:32 +0200

    master changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
-        String tekst = "New message";
+        String tekst2 = "New message";
+        System.out.println(tekst2);
         String message = "Hello!";
         int number1 = 9;
         System.out.println("Täna on teisipäev!");
