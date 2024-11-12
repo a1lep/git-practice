93d4d371f6ed0e650f8a979be8cc40db40fab4f3
a1lep <aile.pent@gmail.com>
2024-11-12 10:22:09 +0200

    Feature branch first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello people!");
+        String text = "New world";
+
+        System.out.println(text);
 
 
     }
