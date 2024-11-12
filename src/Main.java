0405c22f2c4dcc1a9f097d3198189903e4ea6c2a
a1lep <aile.pent@gmail.com>
2024-11-12 11:24:36 +0200

    feature/test - changes

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
         System.out.println("Hello and welcome!");
         String tekst = "New message";
         String message = "Hello!";
+        int number1 = 9;
 
         System.out.println(tekst + message);
-
+        String lause ="Tere-tere!";
         int number = 10;
         System.out.println(number);
     }
